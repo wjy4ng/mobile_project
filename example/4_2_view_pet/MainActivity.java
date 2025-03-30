b8cb86f8d99b6a79830a538de12953cf45ae32b9
양원준 <yang-wonjun@MacBookAir.local>
2025-03-30 16:24:12 +0900

    실습 4-2 업로드

--- example/4_2_view_pet/MainActivity.java
+++ example/4_2_view_pet/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
