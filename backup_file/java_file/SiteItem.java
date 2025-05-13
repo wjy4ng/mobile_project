8cb714364249cda5c9267b403877ffd2129f2dd2
wjyang <aeillou9@naver.com>
2025-05-13 14:04:28 +0900

    Create: RecyclerView 교체를 위한 SiteItem 생성

---
+++ backup_file/java_file/SiteItem.java
@@ -0,0 +1,10 @@
+package com.cookandroid.mobile_project;
+
+public class SiteItem {
+    public String siteName;
+    public String totpCode = "미생성";
+    public SiteItem(String siteName){
+        this.siteName = siteName;
+    }
+}
+
